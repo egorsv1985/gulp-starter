ac1135f27b12b052fbf01c8831c73a3177476327
egorsv1985 <egorvs2009@gmail.com>
2023-05-08 17:53:13 +0300

    start

--- .stylelintrc.js
+++
@@ -1,9 +0,0 @@
-{
-	"extends": "stylelint-config-standard",
-	"rules": {
-	  "at-rule-no-unknown": null,
-	  "declaration-block-trailing-semicolon": null,
-	  "no-descending-specificity": null
-	}
-  };
-  
